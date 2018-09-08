ed0405b116de9621f8aa9d46ae172f3ba1e81abb
Daniel Pfeil <dp@api-studio.de>
2018-09-09 00:52:48 +0200

    improved return type

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -3,13 +3,13 @@ namespace DanielPfeil\ServiceProviderAuthenticator;
 
 interface ServiceProvider
 {
-    public function getApplicationID():string;
-    public function getSessionId():string;
-    public function getIdentityProvider():string;
-    public function getAuthenticationInstant():string;
-    public function getAuthenticationMethod():string;
-    public function getAuthenticationContextClass():string;
-    public function getSessionIndex();
-    public function getPrefix():string;
-    public function getCookieName():string;
+    public function getApplicationID():?string;
+    public function getSessionId():?string;
+    public function getIdentityProvider():?string;
+    public function getAuthenticationInstant():?string;
+    public function getAuthenticationMethod():?string;
+    public function getAuthenticationContextClass():?string;
+    public function getSessionIndex():?string;
+    public function getPrefix():?string;
+    public function getCookieName():?string;
 }
